941c6edd050d5fba8dd8dd50e0bcb8c4c4f10536
sim-on-code <simone.spinelli@students.uniroma2.eu>
2025-07-25 00:34:44 +0200

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,1 +1,6 @@
-'public class Main { public static void main(String[] args){ System.out.println("Hello"); } }' 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java formatting test successful!");
+    }
+}
+
